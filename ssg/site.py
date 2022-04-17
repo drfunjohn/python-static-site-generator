ecfa8239390a9fc3e9a0abc63881ae3ad1e1d94f
Evgeny Sherbakov <e.sherbakov@f5.com>
2022-04-17 19:13:29 +0300

    chore: class Site

---
+++ ssg/site.py
@@ -0,0 +1,23 @@
+from os import wait
+from pathlib import Path
+
+class Site:
+
+    def __init__(self, source, dest):
+        self.source = Path(source)
+        self.dest = Path(dest)
+
+    def create_dir(self, path):
+        """
+        create directory
+        """
+        directory = Path(self.dest /
+                path.relative_to(self.source))
+        directory.mkdir(parents=True, exist_ok=True)
+
+    def build(self):
+        self.dest.mkdir(parents=True, exist_ok=True)
+        for path in self.source.rglob("*"):
+            if path.is_dir():
+                self.create_dir(path)
+
