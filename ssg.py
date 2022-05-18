b9ac51d3ec925ca3af05ca8fc5a2036088d23490
Evgeny Sherbakov <e.sherbakov@f5.com>
2022-05-18 17:43:01 +0300

    chore: implement typer

---
+++ ssg.py
@@ -0,0 +1,14 @@
+import typer
+from ssg import site
+
+
+def main(source="content", dest="dist"):
+    config = {
+            "source": source,
+            "dest": dest
+            }
+
+    new_site = site.Site(config).build()
+
+
+typer.run(main)
