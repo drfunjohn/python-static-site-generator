c570fd5d1f51eba897094cf6758e638ab4148291
Evgeny Sherbakov <e.sherbakov@f5.com>
2022-05-18 17:29:21 +0300

    chore: build() ready

--- ssg/site.py
+++ ssg/site.py
@@ -10,14 +10,18 @@ class Site:
     def create_dir(self, path):
         """
         create directory
+
+        result: new directory path(relative to self.source) in the self.dest
         """
         directory = Path(self.dest /
                 path.relative_to(self.source))
         directory.mkdir(parents=True, exist_ok=True)
 
     def build(self):
+        """
+        build each dir from source in the dest
+        """
         self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
-
