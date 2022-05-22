c43bdaae134a98c097222f8053fe25e1cb835335
Evgeny Sherbakov <e.sherbakov@f5.com>
2022-05-22 17:59:19 +0300

    chore: fix copy()

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -35,11 +35,11 @@ class Parser:
             file.write(content)
 
     def copy(self, path, source, dest ):
-        shutil.copy2(source, dest / path.relative_to(source) )
+        shutil.copy2(path, dest / path.relative_to(source) )
 
 class ResourceParser(Parser):
     extensions = [ ".jpg", ".png", ".gif", ".css", ".html"]
 
     def parse(self, path: Path, source: Path, dest: Path):
-        copy(path, source, dest)
+        self.copy(path, source, dest)
 
